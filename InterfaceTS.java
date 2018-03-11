ef90a944d6574e8889cf7b21a7e3c71f7c66a382
Rubén Morante González <ruben.morante.gonzalez@outlook.com>
2018-03-11 18:01:59 +0100

    Update InterfaceTS.java

--- InterfaceTS.java
+++ InterfaceTS.java
@@ -8,21 +8,21 @@ public interface InterfaceTS {
             FUNCTION, PARAMETERS, VARIABLE
       };
 
-      public boolean InsertaId(EnumType tipo, String id, EnumCommands cmd);
+      public boolean insertaId(EnumType tipo, String id, EnumCommands cmd);
 
-      public Commands BuscaId(String id);
+      public Commands buscaId(String id);
 
-      public boolean InsertaTipo(String id, EnumType tipo);
+      public boolean insertaTipo(String id, EnumType tipo);
 
-      public EnumType DameTipo(String id);
+      public EnumType dameTipo(String id);
 
-      public bool TiposComp(EnumType tipo1, EnumType tipo2);
+      public bool tiposComp(EnumType tipo1, EnumType tipo2);
 
       public bool esMatriz( EnumType tipo );
 
-      public int DaTamaño( EnumType tipo );
+      public int daTamaño( EnumType tipo );
 
-      public EnumType DaTipoDest( EnumType tipo);
+      public EnumType daTipoDest( EnumType tipo);
 
       //ExpTipo -> TBAS || TBAS x TBAS x TBAS || TBAS x TBAS -> TBAS
 

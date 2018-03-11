bfc213680dee2524d3c35a07376efb54d99ac692
Rubén Morante González <ruben.morante.gonzalez@outlook.com>
2018-03-11 18:26:00 +0100

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -6,4 +6,12 @@ public class Commands {
             this.id = id;
             this.tipo = tipo;
       }
+      
+      public EnumType getTipo(){
+      return this.tipo;
+      }
+      
+      public setTipo(EnumType tipo) {
+            this.tipo = tipo;
+      }
 }
